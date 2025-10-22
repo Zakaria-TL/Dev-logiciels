7d675326a793b8f302311226d3cd88c68850315f
zakaria <tlilizakaria10@gmail.com>
2025-10-22 22:12:58 +0100

    fifth commit

--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -1,3 +1,4 @@
+from src import fizzbuzz
 import pytest
 
 
@@ -7,6 +8,16 @@ import pytest
     (3, 'Fizz'),
     (4, '4'),
     (5, 'Buzz'),
+    (6, 'Fizz'),
+    (7, '7'),
+    (8, '8'),
+    (9, 'Fizz'),
+    (10, 'Buzz'),
+    (11, '11'),
+    (12, 'Fizz'),
+    (13, '13'),
+    (14, '14'),
+    (15, 'FizzBuzz'),
 ])
 def test_n_return_actual_value(n, result):
     actual_value = result
